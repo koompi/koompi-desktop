038e86d3ed87cb32ca195968fd7a31b95a2beaf9
leang27 <hangleang127@gmail.com>
2021-03-26 15:12:16 +0700

    [Desktop] update

--- desktop/src/gui/color_config.rs
+++ desktop/src/gui/color_config.rs
@@ -1,14 +1,13 @@
-use iced_wgpu::{
-    Renderer
-};
+use iced_wgpu::Renderer;
 use iced_winit::{
-    text_input, Element, Text, TextInput, Row, Align,
+    text_input, Element, Text, TextInput, Row, Align, Color
 };
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone)]
 pub struct ColorConfigUI {
     color_state: text_input::State,
-    color: String,
+    text: String,
+    color: Color,
 }
 
 #[derive(Debug, Clone)]
@@ -17,21 +16,23 @@ pub enum ColorConfigMsg {
 }
 
 impl ColorConfigUI {
-    pub fn new(color: String) -> Self {
+    pub fn new(color: Color) -> Self {
         Self {
-            color, ..Self::default()
+            color, 
+            text: String::from("sample test"),
+            color_state: text_input::State::new()
         }
     }
 
     pub fn update(&mut self, msg: ColorConfigMsg) {
         match msg {
-            ColorConfigMsg::ColorChanged(val) => self.color = val
+            ColorConfigMsg::ColorChanged(val) => self.text = val
         }
     }
 
     pub fn view(&mut self) -> Element<ColorConfigMsg, Renderer> {
         let lb_color = Text::new("Color: ");
-        let txt_color = TextInput::new(&mut self.color_state, "", &self.color, ColorConfigMsg::ColorChanged).padding(7);
+        let txt_color = TextInput::new(&mut self.color_state, "", &self.text, ColorConfigMsg::ColorChanged).padding(7);
         Row::new().spacing(15).align_items(Align::Center)
             .push(lb_color)
             .push(txt_color)
--- desktop/src/gui/desktop.rs
+++ desktop/src/gui/desktop.rs
@@ -54,7 +54,7 @@ impl Application for Desktop {
     fn background_color(&self) -> Color {
         let bg_conf = self.desktop_conf.background_conf();
         match bg_conf.kind {
-            BackgroundType::Color => hex_to_color(&bg_conf.color_background).unwrap(),
+            BackgroundType::Color => bg_conf.color_background,
             BackgroundType::Wallpaper => Color::TRANSPARENT
         }
     }
@@ -213,26 +213,4 @@ impl Program for Desktop {
         .width(Length::Fill)
         .height(Length::Fill).into()
     }
-}
-
-
-fn hex_to_color(hex: &str) -> Option<Color> {
-    if hex.len() == 7 {
-        let hash = &hex[0..1];
-        let r = u8::from_str_radix(&hex[1..3], 16);
-        let g = u8::from_str_radix(&hex[3..5], 16);
-        let b = u8::from_str_radix(&hex[5..7], 16);
-
-        return match (hash, r, g, b) {
-            ("#", Ok(r), Ok(g), Ok(b)) => Some(Color {
-                r: r as f32 / 255.0,
-                g: g as f32 / 255.0,
-                b: b as f32 / 255.0,
-                a: 1.0,
-            }),
-            _ => None,
-        };
-    }
-
-    None
 }
